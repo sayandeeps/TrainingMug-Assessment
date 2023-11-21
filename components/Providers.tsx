8126bb792d1048519f6a65cbebab7099eb2752a0
Sayandeep <sayandeep11@gmail.com>
2023-11-22 01:14:46 +0530

    Persisted local storage

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,13 +2,24 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
 
 interface Props {
   children: React.ReactNode;
 }
 const Providers = (props: Props) => {
+    let persistor = persistStore(store)
+
   return (
-    <Provider store={store}>{props.children}</Provider>
+    <Provider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+
+        {props.children}
+        </PersistGate>
+    </Provider>
   );
 };
 
--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { savedPostsSlice } from "./features/savedPostsSlice";
+import {persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+  const rootReducer = combineReducers({
+    savedPosts: savedPostsSlice.reducer,
+  })
+   
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    savedPosts: savedPostsSlice.reducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
